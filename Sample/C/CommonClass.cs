247b401577c5a685a6887038964bad3508ab4707
ogoun <ogoun.er@yandex.ru>
2014-08-29 00:06:34 +0400

    First

---
+++ Sample/C/CommonClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C
+{
+    public class CommonClass
+    {
+        public void Run()
+        {
+            Console.WriteLine("Common method was called");
+        }
+    }
+}
