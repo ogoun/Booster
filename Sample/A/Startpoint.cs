b63680af0be245635da064b30125e19f76dd9698
ogoun <ogoun.er@yandex.ru>
2014-08-29 18:17:09 +0400

    Fix PNG

---
+++ Sample/A/Startpoint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+
+namespace A
+{
+    public class Startpoint
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Assembly " + 
+                Assembly.GetExecutingAssembly().FullName + 
+                " correctly loaded and started");
+            new C.CommonClass().Run();
+        }
+    }
+}
---
+++ Sample/B/Startpoint.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reflection;
+
+namespace B
+{
+    public class Startpoint
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Assembly " + Assembly.GetExecutingAssembly().FullName + " correctly loaded and started");
+            new C.CommonClass().Run();
+        }
+    }
+}
